57c0897c236d6392893166c7edecb26d7232736f
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2024-02-12 00:58:41 +0900

    アプリケーションがすでに起動している場合、既存のプロセスを終了させるよう変更 ただWIndowsでしか動作を確認していない マルチプラットフォームで動くとChatGPTは言っていたがほんとかは知らん

--- VisualAxe/VisualAxe/App.axaml.cs
+++ VisualAxe/VisualAxe/App.axaml.cs
@@ -1,6 +1,7 @@
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using System.Diagnostics;
 using VisualAxe.ViewModels;
 using VisualAxe.Views;
 
@@ -8,6 +9,9 @@ namespace VisualAxe
 {
     public partial class App : Application
     {
+        private System.Threading.Mutex mutex;
+        private const string MutexName = "VisualAxeApp";
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -17,7 +21,32 @@ namespace VisualAxe
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow
+				//アプリケーションがすでに起動している場合、既存のプロセスを終了させる
+				bool createdNew;
+                mutex = new System.Threading.Mutex(true, MutexName, out createdNew);
+				if (!createdNew)
+				{
+					Process currentProcess = Process.GetCurrentProcess();
+					foreach (var process in Process.GetProcessesByName(currentProcess.ProcessName))
+					{
+						if (process.Id != currentProcess.Id)
+						{
+							process.Kill();
+						}
+					}
+				}
+
+                //アプリケーション終了時にmutexを開放するよう設定
+                desktop.Exit += (_, _) =>
+				{
+                    if (mutex != null)
+                    {
+                        mutex.ReleaseMutex();
+                        mutex.Close();
+					}
+				};
+
+				desktop.MainWindow = new MainWindow
                 {
                     DataContext = new MainWindowViewModel(),
                 };
